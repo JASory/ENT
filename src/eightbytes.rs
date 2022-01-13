11c1afd0c4aa1eb6fddb1f9f80b2c1f62afa0352
Rust-CAS <87502258+JASory@users.noreply.github.com>
2022-01-13 00:48:24 -0700

    Update eightbytes.rs

--- src/eightbytes.rs
+++ src/eightbytes.rs
@@ -1,7 +1,7 @@
-use number_theory::traits::NumberTheory;
-use number_theory::primes::PRIMELIST;
+use crate::traits::NumberTheory;
+use crate::primes::PRIMELIST;
 
-use number_theory::fjprime64::fjprime_64;
+use crate::fjprime64::fjprime_64;
 
 
 
