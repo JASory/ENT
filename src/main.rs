7a9eba3639514ad8546324e710dac88b59464a05
Rust-CAS <87502258+JASory@users.noreply.github.com>
2022-01-13 00:44:14 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -23,40 +23,6 @@ pub mod sixteenbytes;
  
 
 
-fn hardware_rng()-> u64{
-    let mut x: u64 = 0; 
-    let k = unsafe { core::arch::x86_64::_rdrand64_step(&mut x) } ;
-   x
- }
-  
-  
-  fn main(){
-  // 203280221primes counted in 900.660736901s // 203280221 203152305.728129     999897657631
-  // 203280221primes counted in 549.946332932s
-  //4506732primes counted in 38.294082118s
-
-  // 2^86243-1 in  2276.469896756s
-  let t = Mpz::from_u64(10);
-   let mut count = 0u64;
-   let mut k = Mpz::new(Sign::Positive, vec![u64::MAX;2]);
-   k.mut_addition(t);
-   //println!("{}",k.to_string());
-   
-   
-let time = std::time::Instant::now();
-for i in 0..300_000{
- if k.probable_prime(){
-  count+=1;
-  //println!("13^ 8258 + {} is prime",i+10);
- }
- //println!("Evaluated 13^ 8258 + {}",i);
- k.successor();
-}
-let stop = time.elapsed();
-println!("{:?}primes counted in {:?}",count, stop)
-  
-  }
-
 
 
 
