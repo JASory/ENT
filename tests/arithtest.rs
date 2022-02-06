32b142e2048e39f3abf881470e2f1ce906d0dad7
Rust-CAS <jasory@rust-cas.org>
2022-02-06 11:37:29 -0700

    Add files via upload

---
+++ tests/arithtest.rs
@@ -0,0 +1,85 @@
+use number_theory::Mpz;
+use number_theory::Sign; 
+ //#[ignore]
+ #[test]
+ 
+ fn arith(){
+ 
+ assert_eq!(Mpz::zero().ref_addition(&Mpz::u_from_string("222222222222222222222").unwrap()).to_string(), "222222222222222222222");
+ assert_eq!(Mpz::one().ref_addition(&Mpz::u_from_string("222222222222222222222").unwrap()).to_string(), "222222222222222222223");
+ 
+ assert_eq!(Mpz::u_from_string("222222222222222222222").unwrap().ref_addition(&Mpz::u_from_string("222222222222222222223").unwrap()).to_string(), "444444444444444444445");
+ assert_eq!(Mpz::u_from_string("222222222222222222222").unwrap().ref_addition(&
+ Mpz::u_from_string("222222222222222222222222222222222222222222").unwrap()).to_string(), "222222222222222222222444444444444444444444");
+ 
+ assert_eq!(Mpz::u_from_string("222222222222222222222222222222222222222222").unwrap().ref_addition(&
+ Mpz::u_from_string("222222222222222222222").unwrap()).to_string(), "222222222222222222222444444444444444444444");
+ 
+ assert_eq!(Mpz::from_string(Sign::Negative,"222222222222222222222222222222222222222222").unwrap().ref_addition(&
+ Mpz::u_from_string("222222222222222222222").unwrap()).to_string(), "-222222222222222222222000000000000000000000");
+ 
+ assert_eq!(Mpz::from_string(Sign::Negative,"222222222222222222222222222222222222222222").unwrap().ref_addition(&
+ Mpz::from_string(Sign::Negative,"222222222222222222222").unwrap()).to_string(), "-222222222222222222222444444444444444444444");
+ 
+ assert_eq!(Mpz::from_string(Sign::Positive,"222222222222222222222222222222222222222222").unwrap().ref_addition(&
+ Mpz::from_string(Sign::Negative,"222222222222222222222").unwrap()).to_string(), "222222222222222222222000000000000000000000");
+ 
+  // Product                                                                                                   
+ assert_eq!(Mpz::zero().ref_product(&Mpz::u_new(vec![1,2])),Mpz::zero());
+ assert_eq!(Mpz::one().ref_product(&Mpz::u_new(vec![1,2])),Mpz::u_new(vec![1,2]));
+ 
+ assert_eq!(Mpz::one().ref_product(&Mpz::u_new(vec![1,2])),Mpz::u_new(vec![1,2,0]));
+  
+ assert_eq!(Mpz::u_from_string("222222222222222222222").unwrap().ref_product(&Mpz::u_from_string("222222222222222222222").unwrap()).to_string(),
+ "49382716049382716049283950617283950617284" ) ;
+ 
+ assert_eq!(Mpz::from_string(Sign::Negative,"222222222222222222222").unwrap().ref_product(&Mpz::from_string(Sign::Negative,"222222222222222222222").unwrap()).to_string(),
+ "49382716049382716049283950617283950617284") ;
+ 
+ assert_eq!(Mpz::from_string(Sign::Positive,"222222222222222222222").unwrap().ref_product(&Mpz::from_string(Sign::Negative,"222222222222222222222").unwrap()).to_string(),
+ "-49382716049382716049283950617283950617284") ;
+ 
+ assert_eq!(Mpz::from_string(Sign::Negative,"222222222222222222222").unwrap().ref_product(&Mpz::from_string(Sign::Positive,"222222222222222222222").unwrap()).to_string(),
+ "-49382716049382716049283950617283950617284") ;
+ // exponentiation
+ assert_eq!(Mpz::from_i64(33).pow(42).to_string(), "5992188611668647482797702362673386216879593110712943882639341889");
+ assert_eq!(Mpz::from_i64(-33).pow(42).to_string(), "5992188611668647482797702362673386216879593110712943882639341889");
+ assert_eq!(Mpz::from_i64(-33).pow(43).to_string(), "-197742224185065366932324177968221745157026572653527148127098282337");
+ 
+ 
+ 
+ }
+ 
+ // #[ignore]
+ #[test]
+ 
+ fn math(){
+ 
+ assert_eq!(Mpz::from_u64(33).pow(43).sqrt().to_string(),"444682160857690992367099165368543"); // sqrt
+ assert_eq!(Mpz::from_u64(33).pow(43).ln(),150.34982514305864);// natural logarithm
+ assert_eq!(Mpz::from_u64(33).pow(43).log(2.718281828459045235),150.34982514305864);// natural logarithm
+ 
+ //assert_eq!(Mpz::from_u64(2).shl(65536).iter_log(2.0),4);
+ 
+ 
+ }
+ //#[ignore]
+ #[test] 
+ 
+ fn bitwise(){
+ // Shift-right ops
+ assert_eq!(Mpz::from_u64(1).shl(0).to_u64().unwrap(),1);
+ assert_eq!(Mpz::from_u64(1).shl(1).to_u64().unwrap(),2);
+ assert_eq!(Mpz::from_u64(1).shl(65).to_u128().unwrap(),36893488147419103232);
+ assert_eq!(Mpz::u_from_string("2864165568461618616184867").unwrap().shl(65).to_string(),"105669058452284624487376314655265894269190144");
+ 
+ assert_eq!(Mpz::from_u64(1).shr(0).to_u64().unwrap(), 1);
+ assert_eq!(Mpz::from_u64(1).shr(1).to_u64().unwrap(), 0);
+ assert_eq!(Mpz::from_u64(1).shr(15).to_u64().unwrap(), 0);
+ assert_eq!(Mpz::from_u128(36893488147419103232).shr(65).to_u64().unwrap(),1);
+ 
+ assert_eq!(Mpz::from_u64(16841684685).and(&Mpz::from_u64(1854618418)).to_u64().unwrap(),1786982912);
+ assert_eq!(Mpz::from_u64(16841684685).or(&Mpz::from_u64(1854618418)).to_u64().unwrap(),16909320191);
+ assert_eq!(Mpz::from_u64(16841684685).xor(&Mpz::from_u64(1854618418)).to_u64().unwrap(),15122337279);
+ }
+ 
---
+++ tests/enttest.rs
@@ -0,0 +1,89 @@
+ use number_theory::Mpz;
+ use number_theory::NumberTheory;
+ use number_theory::Sign; 
+   
+   //#[ignore]
+   #[test]
+    fn primality(){
+    
+    
+    assert_eq!(0u8.is_prime(),false);
+    assert_eq!(2u8.is_prime(),true);
+    
+     assert_eq!(0i8.is_prime(),false);
+    assert_eq!(2i8.is_prime(),true);
+    
+    
+    
+    //assert_eq!()
+    assert_eq!(Mpz::u_new(vec![
+14125993214864411435, 17627421051211808665, 8803062915072472141, 16592776965248599063,
+2400842081300231610, 15499637576960551660, 13635884493322354475, 11162303294968070407, 
+16638949889857351534, 4755447895431218293, 3008559606663544016, 6962752268970074012, 
+15476370627519396605, 7818823728409314089, 7726208862163452462, 2793947945622957183, 
+9898516143788448943, 2723958373070846137, 15945921893246334102, 9257474160255769327, 138699416178]).probable_prime(), false);
+
+  let mut mersenne = Mpz::from_u64(1).shl(82589933);  //1290467  44
+     mersenne.mut_subtraction(Mpz::one());
+     
+     assert_eq!(mersenne.probable_prime(),true)
+    
+    }
+    
+    //#[ignore]
+    #[test]
+    fn euclidean_div(){
+    assert_eq!(17i8.euclidean_div(&5i8).0 * 5i8 + (17i8.euclidean_div(&5i8).1), 17i8);
+    assert_eq!(17i8.euclidean_div(&-5i8).0 * -5i8 + (17i8.euclidean_div(&-5i8).1), 17i8);
+    assert_eq!(-17i8.euclidean_div(&-5i8).0 * -5i8 + (-17i8.euclidean_div(&-5i8).1), -17i8);
+    assert_eq!(-17i8.euclidean_div(&5i8).0 * 5i8 + (-17i8.euclidean_div(&5i8).1), -17i8);
+    
+    assert_eq!(17i16.euclidean_div(&5i16).0 * 5i16 + (17i16.euclidean_div(&5i16).1), 17i16);
+    assert_eq!(17i16.euclidean_div(&-5i16).0 * -5i16 + (17i16.euclidean_div(&-5i16).1), 17i16);
+    assert_eq!(-17i16.euclidean_div(&-5i16).0 * -5i16 + (-17i16.euclidean_div(&-5i16).1), -17i16);
+    assert_eq!(-17i16.euclidean_div(&5i16).0 * 5i16 + (-17i16.euclidean_div(&5i16).1), -17i16);
+    
+    assert_eq!(17i32.euclidean_div(&5i32).0 * 5i32 + (17i32.euclidean_div(&5i32).1), 17i32);
+    assert_eq!(17i32.euclidean_div(&-5i32).0 * -5i32 + (17i32.euclidean_div(&-5i32).1), 17i32);
+    assert_eq!(-17i32.euclidean_div(&-5i32).0 * -5i32 + (-17i32.euclidean_div(&-5i32).1), -17i32);
+    assert_eq!(-17i32.euclidean_div(&5i32).0 * 5i32 + (-17i32.euclidean_div(&5i32).1), -17i32);
+    
+    assert_eq!(17i64.euclidean_div(&5i64).0 * 5i64 + (17i64.euclidean_div(&5i64).1), 17i64);
+    assert_eq!(17i64.euclidean_div(&-5i64).0 * -5i64 + (17i64.euclidean_div(&-5i64).1), 17i64);
+    assert_eq!(-17i64.euclidean_div(&-5i64).0 * -5i64 + (-17i64.euclidean_div(&-5i64).1), -17i64);
+    assert_eq!(-17i64.euclidean_div(&5i64).0 * 5i64 + (-17i64.euclidean_div(&5i64).1), -17i64);
+    
+    assert_eq!(17i128.euclidean_div(&5i128).0 * 5i128 + (17i128.euclidean_div(&5i128).1), 17i128);
+    assert_eq!(17i128.euclidean_div(&-5i128).0 * -5i128 + (17i128.euclidean_div(&-5i128).1), 17i128);
+    assert_eq!(-17i128.euclidean_div(&-5i128).0 * -5i128 + (-17i128.euclidean_div(&-5i128).1), -17i128);
+    assert_eq!(-17i128.euclidean_div(&5i128).0 * 5i128 + (-17i128.euclidean_div(&5i128).1), -17i128);
+    
+    
+    assert_eq!(17i16.euclidean_div(&5i16).0 * 5i16 + (17i16.euclidean_div(&5i16).1), 17i16);
+    assert_eq!(17i16.euclidean_div(&-5i16).0 * -5i16 + (17i16.euclidean_div(&-5i16).1), 17i16);
+    assert_eq!(-17i16.euclidean_div(&-5i16).0 * -5i16 + (-17i16.euclidean_div(&-5i16).1), -17i16);
+    assert_eq!(-17i16.euclidean_div(&5i16).0 * 5i16 + (-17i16.euclidean_div(&5i16).1), -17i16);
+    
+    let (quo, rem) = Mpz::from_i64(17).euclidean_div(&Mpz::from_i64(5));
+    assert_eq!( (quo.ref_product(&Mpz::from_i64(5))).ref_addition(&rem), Mpz::from_i64(17));
+    
+    let (quo, rem) = Mpz::from_i64(-17).euclidean_div(&Mpz::from_i64(5));
+    assert_eq!( (quo.ref_product(&Mpz::from_i64(5))).ref_addition(&rem), Mpz::from_i64(-17));
+    
+    
+     let (quo, rem) = Mpz::from_i64(-17).euclidean_div(&Mpz::from_i64(-5));
+    assert_eq!( (quo.ref_product(&Mpz::from_i64(-5))).ref_addition(&rem), Mpz::from_i64(-17));
+    
+     let (quo, rem) = Mpz::from_i64(17).euclidean_div(&Mpz::from_i64(-5));
+    assert_eq!( (quo.ref_product(&Mpz::from_i64(-5))).ref_addition(&rem), Mpz::from_i64(17));
+    // i128                                              
+    let (quo, rem) = Mpz::from_i128(17).euclidean_div(&Mpz::from_i128(5));
+    assert_eq!( (quo.ref_product(&Mpz::from_i128(5))).ref_addition(&rem), Mpz::from_i128(17));
+    
+    let (quo, rem) = Mpz::from_i128(-170000000000000000000).euclidean_div(&Mpz::from_i128(50000000));
+    assert_eq!( (quo.ref_product(&Mpz::from_i128(50000000))).ref_addition(&rem), Mpz::from_i128(-170000000000000000000));
+    
+    }
+    
+   
+   
