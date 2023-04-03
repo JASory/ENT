fca294f26e920f3a74cf3d704560d39b17905ef9
Rust-CAS <jasory@rust-cas.org>
2023-04-02 17:24:05 -0700

    Update other files to use new API

--- examples/rsa.rs
+++ examples/rsa.rs
@@ -3,7 +3,7 @@ use number_theory::NumberTheory;
 
 /*
 
- Naive implementation of RSA cryptosystem in k-bits, works for k greater than 256 and
+ Naive implementation of RSA cryptosystem in k-bits, works for k greater than 256
 
  This is not a cryptographic library and this implementation is not secure.
 
@@ -16,7 +16,7 @@ fn gen_safe(k: u32) -> Mpz {
     // Generate safe primes to minimize the factors of p-1 using standard primes are much faster
 
     loop {
-        let p = Mpz::prime_gen(k);
+        let p = Mpz::prime_gen(k).unwrap();
         if p.congruence_u64(3, 2) {
             match p.is_sophie() {
                 Some(x) => return x,
@@ -35,16 +35,16 @@ fn generate_keys(k: u32) -> (Mpz, Mpz, Mpz) {
         lo -= 1;
     }
     let p = gen_safe(hi);
-    //  println!("p = {}", p.to_string());
+
     let q = gen_safe(lo);
-    //println!("q ={}", q.to_string());
+
     let phi = p
         .ref_subtraction(&Mpz::one())
         .ref_product(&q.ref_subtraction(&Mpz::one()));
     let n = p.ref_product(&q);
     assert_eq!(n.bit_length(), k as u64); // checks that the key is exactly k-bits
 
-    let e = Mpz::from_u64(u64::prime_gen(64)); // generates exponent between 2^63;2^64
+    let e = Mpz::from_u64(u64::prime_gen(64).unwrap()); // generates exponent between 2^63;2^64
     let mut inv = e.extended_gcd(&phi).1;
     if !inv.is_positive() {
         inv = phi.ref_addition(&inv);
@@ -54,7 +54,7 @@ fn generate_keys(k: u32) -> (Mpz, Mpz, Mpz) {
 }
 
 fn main() {
-    const STRENGTH: u32 = 1024;
+    const STRENGTH: u32 = 512;
 
     let secret = Mpz::from_u64(0x29A822A1AA29229 << 5);
     let start = std::time::Instant::now();
@@ -65,7 +65,7 @@ fn main() {
     let decrypted = encrypted.exp_residue(&private, &public);
     println!("{:?} for a key of {}-bits \n ", stop, STRENGTH);
     println!(
-        "{}",
+        "The secret is . . . \n {}",
         String::from_utf8(decrypted.to_u64().unwrap().to_be_bytes().to_vec()).unwrap()
     );
 }
